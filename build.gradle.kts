c82b736f83e17274405d48631f8a4aea9dfc6eb1
cheroliv <cheroliv@laposte.net>
2026-04-01 13:16:14 +0200

    🧹 chore(build.gradle.kts): remove unused plugin aliases and slider configuration for cleaner setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,9 @@
 plugins {
-    alias(libs.plugins.slider)
-    alias(libs.plugins.readme)
     id("com.cheroliv.plantuml") version "0.0.1"
 }
 
 plantuml {
     configPath = "plantuml-context.yml"
-        .run(::file)
-        .absolutePath
-}
-
-slider {
-    configPath = "slides-context.yml"
         .run(::file)
         .absolutePath
 }
