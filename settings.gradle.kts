7280b5119b32a6bc91bb9eb22c168d96068f4ac5
cheroliv <cheroliv@laposte.net>
2026-04-01 13:14:53 +0200

    🔧 chore(settings.gradle.kts): add mavenCentral to plugin management repositories and include plantuml-plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,10 @@
-pluginManagement.repositories {
-    mavenLocal()
-    gradlePluginPortal()
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+        mavenCentral()
+    }
 }
+
 rootProject.name = "plantuml-gradle"
+include(":plantuml-plugin")
