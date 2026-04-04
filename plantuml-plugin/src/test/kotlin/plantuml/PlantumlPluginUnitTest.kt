9f37fabf536af555bedfece716300557b8016206
cheroliv <cheroliv@laposte.net>
2026-04-04 08:09:05 +0200

    🐛 fix(PlantumlPluginUnitTest): clean up unnecessary whitespace in test methods for better readability

--- plantuml-plugin/src/test/kotlin/plantuml/PlantumlPluginUnitTest.kt
+++ plantuml-plugin/src/test/kotlin/plantuml/PlantumlPluginUnitTest.kt
@@ -4,11 +4,10 @@ import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionContainer
 import org.gradle.api.plugins.PluginContainer
 import org.gradle.api.tasks.TaskContainer
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.mockito.Mockito.*
 import kotlin.test.assertNotNull
-import kotlin.test.assertTrue
 
 class PlantumlPluginUnitTest {
 
@@ -25,7 +24,7 @@ class PlantumlPluginUnitTest {
         extensions = mock(ExtensionContainer::class.java)
         plugins = mock(PluginContainer::class.java)
         tasks = mock(TaskContainer::class.java)
-        
+
         // Configure mock behaviors
         `when`(project.extensions).thenReturn(extensions)
         `when`(project.plugins).thenReturn(plugins)
@@ -36,10 +35,10 @@ class PlantumlPluginUnitTest {
     fun `should register plantuml extension when plugin is applied`() {
         // Given
         plugin = PlantumlPlugin()
-        
+
         // When
         plugin.apply(project)
-        
+
         // Then
         verify(extensions).create("plantuml", PlantumlPlugin.PlantumlExtension::class.java)
     }
@@ -48,10 +47,10 @@ class PlantumlPluginUnitTest {
     fun `should register all required tasks when plugin is applied`() {
         // Given
         plugin = PlantumlPlugin()
-        
+
         // When
         plugin.apply(project)
-        
+
         // Then
         verify(tasks).register("processPlantumlPrompts", plantuml.tasks.ProcessPlantumlPromptsTask::class.java)
         verify(tasks).register("validatePlantumlSyntax", plantuml.tasks.ValidatePlantumlSyntaxTask::class.java)
@@ -63,7 +62,7 @@ class PlantumlPluginUnitTest {
         // Given
         val objects = mock(org.gradle.api.model.ObjectFactory::class.java)
         val property = mock(org.gradle.api.provider.Property::class.java)
-        
+
         // This test is more of a compile-time check since we can't easily mock the internal Gradle structures
         // But it ensures the extension class can be instantiated
         val extension = PlantumlPlugin.PlantumlExtension(objects)
