a8e60803e449eaaa4d660c55989f7f24a9de5159
cheroliv <cheroliv@laposte.net>
2026-04-10 15:42:54 +0200

    Split `@ParameterizedTest` into distinct `@Test` methods for clearer handshake validation logic.

--- plantuml-plugin/src/functionalTest/kotlin/plantuml/MegaOptimizedFunctionalTest.kt
+++ plantuml-plugin/src/functionalTest/kotlin/plantuml/MegaOptimizedFunctionalTest.kt
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import java.io.File
 import kotlin.test.assertTrue
-import kotlin.test.Ignore
 
 class MegaOptimizedFunctionalTest {
 
