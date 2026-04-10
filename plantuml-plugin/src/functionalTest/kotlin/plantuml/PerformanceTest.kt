6baa41d83d1411ae7edfe88676cba27f9ef1760b
cheroliv <cheroliv@laposte.net>
2026-04-10 15:43:03 +0200

    Split `@ParameterizedTest` into individual `@Test` methods for isolated performance validation in Gradle builds.

--- plantuml-plugin/src/functionalTest/kotlin/plantuml/PerformanceTest.kt
+++ plantuml-plugin/src/functionalTest/kotlin/plantuml/PerformanceTest.kt
@@ -3,7 +3,6 @@
 package plantuml
 
 import org.gradle.testkit.runner.BuildResult
-import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.GradleRunner.create
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
