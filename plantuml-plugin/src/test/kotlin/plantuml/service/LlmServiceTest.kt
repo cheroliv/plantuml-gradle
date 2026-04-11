c2838ecec33fab3763a35ac902ee381cbf2a5d2e
cheroliv <cheroliv@laposte.net>
2026-04-11 03:16:32 +0200

    Refine `LlmServiceTest` by consolidating tests and reducing boilerplate for cleaner parameterized execution.

--- plantuml-plugin/src/test/kotlin/plantuml/service/LlmServiceTest.kt
+++ plantuml-plugin/src/test/kotlin/plantuml/service/LlmServiceTest.kt
@@ -1,6 +1,5 @@
 package plantuml.service
 
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EnumSource
 import plantuml.PlantumlConfig
